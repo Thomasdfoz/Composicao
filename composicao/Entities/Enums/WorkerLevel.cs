33f39334d6d69df13611d0efca755dc255641927
Thomas Barros <thomas_sp18@hotmail.com>
2019-02-03 13:39:55 -0200

    Composicao

---
+++ composicao/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace composicao.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior,
+        Mid_Level,
+        Senior
+    }
+}
